259fd120d17adc2aa6228d72592092f0fed42373
rankalpha <52857054+rankalpha@users.noreply.github.com>
2025-05-22 00:30:56 +0800

    Update msm_vidc_common.c

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -712,8 +712,6 @@ static void handle_sys_init_done(enum hal_command_response cmd, void *data)
 	return;
 }
 
-static void put_inst(struct msm_vidc_inst *inst)
-{
 	void put_inst_helper(struct kref *kref)
 	{
 		struct msm_vidc_inst *inst = container_of(kref,
@@ -722,6 +720,8 @@ static void put_inst(struct msm_vidc_inst *inst)
 		msm_vidc_destroy(inst);
 	}
 
+static void put_inst(struct msm_vidc_inst *inst)
+{
 	if (!inst)
 		return;
 
