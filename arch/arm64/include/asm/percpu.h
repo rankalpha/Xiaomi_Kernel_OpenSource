cdc07518b6f3e8ea23f20fe988be9c842937450a
rankalpha <52857054+rankalpha@users.noreply.github.com>
2025-05-28 23:15:12 +0800

    Update percpu.h

--- arch/arm64/include/asm/percpu.h
+++ arch/arm64/include/asm/percpu.h
@@ -42,7 +42,8 @@ static inline unsigned long __my_cpu_offset(void)
 static inline unsigned long __percpu_##op(void *ptr,			\
 			unsigned long val, int size)			\
 {									\
-	unsigned long loop, ret;					\
+	unsigned long loop = 0;                                         \
+        unsigned long ret = 0;   					\
 									\
 	switch (size) {							\
 	case 1:								\
@@ -100,7 +101,7 @@ PERCPU_OP(or, orr)
 
 static inline unsigned long __percpu_read(void *ptr, int size)
 {
-	unsigned long ret;
+	unsigned long ret = 0;
 
 	switch (size) {
 	case 1:
@@ -146,7 +147,8 @@ static inline void __percpu_write(void *ptr, unsigned long val, int size)
 static inline unsigned long __percpu_xchg(void *ptr, unsigned long val,
 						int size)
 {
-	unsigned long ret, loop;
+	unsigned long ret = 0;
+        unsigned long loop = 0;
 
 	switch (size) {
 	case 1:
